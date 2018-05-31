63c035d8fa0607fd40692e929520eb14247df354
RadoszSzymon <radoszszymon@gmail.com>
2018-05-31 21:13:21 +0200

    mobile styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,12 @@
     <div class="col-sm-12">
         <img src="{{ URL::to('/') }}/images/logo2.png" class="logo">
         <!--<h2>Take a ride.</h2>-->
+
+        <div class="welcomeText">
+            <p>Hitchy is free application for hitchhikers. Find the best spots in your area. </p>
+            <p>You can also find gas stations, stores and more.</p>
+            <p> Are you going to a new place and are you looking for companions? </br>You can do it using hitchy.</p>
+        </div>
     
         <div class="buttons">
             <a href="{{url('/login')}}"><div class="btn btn-default signIn">SIGN IN</div></a>
